7e0500075979a782326232903f575bd35bb4f3b8
dmitrypenzar1996 <dmitry_penzar_1996@mail.ru>
2025-11-28 02:22:19 +0300

    add legnet main code

---
+++ chorus/oracles/legnet/dataset.py
@@ -0,0 +1,57 @@
+import torch.nn as nn
+import math
+
+from torch.utils.data import Dataset 
+from .transforms import Seq2Tensor, AddFlanks, PadNs, ReverseComplement
+from .legnet_globals import LEGNET_WINDOW, LEGNET_STEP
+from .agarwal_meta import LEFT_MPRA_FLANK, RIGHT_MPRA_FLANK
+
+
+class SubSequenceDataset(Dataset):
+    def __init__(self, 
+                 sequence: str, 
+                 reverse_aug: bool = True,
+                 window_size: int = LEGNET_WINDOW, 
+                 step: int = LEGNET_STEP, 
+                 left_flank: str = LEFT_MPRA_FLANK,
+                 right_flank: str = RIGHT_MPRA_FLANK):
+        """
+            sequence: A single nucleotide sequence (e.g., 'ATGC...')
+            window_size: Length of each subsequence.
+            step: Step size to slide the window.
+        """
+        self.sequence = sequence
+
+        self.window_size = window_size
+        self.step = step
+        self.transform = nn.Sequential(AddFlanks(up=left_flank,
+                                               down=right_flank),
+                                      PadNs(size=self.window_size),
+                                      Seq2Tensor())
+        self.rev_compl = ReverseComplement()
+
+        self.reverse_aug = reverse_aug
+        # Calculate number of valid subsequences
+        if len(self.sequence) <= self.window_size:
+            self.num_samples = 1 
+        else:
+            self.num_samples = math.ceil(len(self.sequence) / step)
+
+    def __len__(self):
+        return self.num_samples
+
+    def _encode(self, subseq: str):
+        if self.transform is not None:
+            subseq_enc = self.transform(subseq)
+        return subseq_enc
+
+    def __getitem__(self, idx):
+        start = idx * self.step
+        end = start + self.window_size
+        subseq = self.sequence[start:end]   
+       
+        if self.reverse_aug:
+            rev_subseq = self.rev_compl(subseq)
+            return self._encode(subseq), self._encode(rev_subseq), start
+        else:
+            return self._encode(subseq), start
---
+++ chorus/oracles/legnet/transforms.py
@@ -0,0 +1,94 @@
+import numpy as np 
+
+import torch 
+import torch.nn as nn 
+import torch.nn.functional as F
+
+CODES = {
+    "A": 0,
+    "T": 3,
+    "G": 1,
+    "C": 2,
+    'N': 4
+}
+
+INV_CODES = {value: key for key, value in CODES.items()}
+
+COMPL = {
+    'A': 'T',
+    'T': 'A',
+    'G': 'C',
+    'C': 'G',
+    'N': 'N'
+}
+
+def n2id(n):
+    return CODES[n.upper()]
+
+def id2n(i):
+    return INV_CODES[i]
+
+
+class Seq2Tensor(nn.Module):
+    '''
+    Encode sequences using one-hot encoding after preprocessing.
+    '''
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, seq: str):
+        seq_code = [n2id(x) for x in seq]
+        code = torch.from_numpy(np.array(seq_code))
+        code = F.one_hot(code, num_classes=5) # 5th class is N
+
+        mask = code[:, 4] == 1
+        code = code[:, :4].float() 
+        code[mask] = 0.25 # encode Ns with .25
+        return code.transpose(0, 1)
+
+
+class AddFlanks(nn.Module):
+    '''
+    Add flanks to nucleotide sequence
+    '''
+
+    def __init__(self, 
+                 up: str, 
+                 down: str):
+        super().__init__()
+        self.up = up 
+        self.down = down
+
+    def forward(self, seq: str):
+        return self.up + seq + self.down
+
+    def __repr__(self):
+        return f"AddFlanks(up={self.up}, down={self.down})"
+
+
+class PadNs(nn.Module):
+    '''
+    Pad sequence with Ns up to requested size
+    '''
+
+    def __init__(self, 
+                 size: int = 200):
+        super().__init__()
+        self.size = size
+
+    def forward(self, seq: str):
+        add_sz = self.size - len(seq)
+        lp, mod = divmod(add_sz, 2)
+        lp, rp = lp + mod, lp
+        return lp * 'N' + seq + rp * 'N'
+
+
+class ReverseComplement(nn.Module):
+    '''
+    Get reverse complement of the sequence
+    '''
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, seq: str):
+        return ''.join(COMPL[c] for c in reversed(seq))
